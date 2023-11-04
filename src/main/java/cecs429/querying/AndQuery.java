0447ee5c177c5947738ccf57e3158fb59b07bacf
joe-schmoe4 <daedricgun@gmail.com>
2023-11-04 00:13:07 -0700

    Implemented intersection for boolean query

--- src/main/java/cecs429/querying/AndQuery.java
+++ src/main/java/cecs429/querying/AndQuery.java
@@ -1,5 +1,6 @@
 package cecs429.querying;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -23,10 +24,37 @@ public class AndQuery implements QueryComponent {
 	
 	@Override
 	public List<Posting> getPostings(Index index) {
-		List<Posting> result = null;
-		
-		// TODO: program the merge for an AndQuery, by gathering the postings of the composed QueryComponents and
-		// intersecting the resulting postings.
+		List<Posting> result = new ArrayList<>();
+
+		for (int i=0; i<mComponents.size()-1; i++){
+			List<Posting> list1 = index.getPostings(mComponents.get(i).toString());
+			List<Posting> list2 = index.getPostings(mComponents.get(i+1).toString());
+
+
+			int index1 = 0;
+			int index2 = 0;
+			// Uses two pointer algorithm where an index counter will advance only if the doc id of a list is smaller than the other
+			while (true){
+				if (list1.get(index1).getDocumentId() > list2.get(index2).getDocumentId()){
+					index2 += 1;
+				}
+				else if (list1.get(index1).getDocumentId() < list2.get(index2).getDocumentId()){
+					index1 += 1;
+				}
+				// Both doc ids are equal therefore add it to result list and advance both index counters
+				else{
+					result.add(list1.get(index1));
+					index1 += 1;
+					index2 += 1;
+					if (index1 == list1.size() || index2 == list2.size()){
+						break;
+					}
+				}
+			}
+		}
+
+
+
 		
 		return result;
 	}
--- src/main/java/edu/csulb/PositionalInvertedIndexer.java
+++ src/main/java/edu/csulb/PositionalInvertedIndexer.java
@@ -6,6 +6,8 @@ import cecs429.documents.DocumentCorpus;
 import cecs429.indexing.Index;
 import cecs429.indexing.PositionalInvertedIndex;
 import cecs429.indexing.Posting;
+import cecs429.querying.BooleanQueryParser;
+import cecs429.querying.QueryComponent;
 import cecs429.text.EnglishTokenStream;
 import cecs429.text.TokensProcessor;
 
@@ -19,6 +21,8 @@ public class PositionalInvertedIndexer {
 
         long start = System.currentTimeMillis();
         // Create a DocumentCorpus to load .txt or .json documents from the project directory.
+
+
         System.out.print("Input: \"txt\" or \"json\": ");
         String input = obj.nextLine();
 
@@ -29,7 +33,7 @@ public class PositionalInvertedIndexer {
         else if (input.equals("json")){
             corpus = DirectoryCorpus.loadTextDirectory(Paths.get("/home/joe/IdeaProjects/searchengine/nps-sites").toAbsolutePath(), ".json");
         }
-        
+
         // Index the documents of the corpus.
         Index index = indexCorpus(corpus) ;
 
@@ -38,6 +42,7 @@ public class PositionalInvertedIndexer {
         System.out.println("Time took: " + (finish - start)*.001 + " seconds");
         // Takes user input and queries for word. Enter "quit" to stop.
         while (true) {
+            BooleanQueryParser parser = new BooleanQueryParser();
             System.out.print("Enter a word in lowercase to be queried: ");
             String query = obj.nextLine();
 
@@ -45,8 +50,10 @@ public class PositionalInvertedIndexer {
                 break;
             }
 
+            QueryComponent parsed = parser.parseQuery(query);
+
             int foundCount = 0;
-            for (Posting p : index.getPostings(query)) {
+            for (Posting p : parsed.getPostings(index)) {
                 System.out.println(query + " found in " + corpus.getDocument(p.getDocumentId()).getTitle());
                 foundCount += 1;
             }
